dcf794ed0eed21d1cd032089ba6772483459c18e
Jeremy Carter <defcron@gptenv.com>
2025-06-05 14:25:50 +0200

    Update .bootstrap.sh

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -e
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove 
+sudo apt-get install -y apt-utils nodejs
+pushd backend
+npm i
+popd
 curl -fsSL https://get.docker.com/ | sh
 cp .env.example .env
 chmod 600 .env
@@ -9,4 +13,10 @@ export CHARGPT_ACCESS_TOKEN=""
 export CHARGPT_API_BASE=""
 unset CHARGPT_ACCESS_TOKEN
 unset CHARGPT_API_BASE
-./up.sh
+sudo gpasswd -a $(whoami) docker
+sudo dockerd &
+sleep 15
+docker ps || echo "ERROR: dockerd not running or not accessible (see line above)"
+docker build -t gptenv/chargpt-backend:latest .
+docker build -t gptenv/chargpt-warp:latest .
+docker exec -it chargpt-backend '/bin/bash -c "node backend/dev/test-chat-completions.js"'
